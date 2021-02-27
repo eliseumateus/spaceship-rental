f08148e3f9035b12d4806d6b9aa6836d14980347
Eliseu Mateus <eliseumateus@gmail.com>
2021-02-27 19:10:47 +0000

    Now admin can add trips for any destination - no restrictions

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -4,7 +4,6 @@ class Spaceship < ApplicationRecord
   has_one_attached :photo
 
   validates :name,  presence: true, uniqueness: true
-  validates :destination, inclusion: { in: %w(mars saturn mercury venus uranus neptune moon), message: "Please type a valid Planet!" }
   validates :price, numericality: { only_integer: true }
 
   include PgSearch::Model
